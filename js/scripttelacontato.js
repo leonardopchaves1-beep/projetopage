476f78b3b2168b77c13e72e2450b385bb6a638dd
LEONARDO PINTO CHAVES <leonardo.pchaves@senacsp.edu.br>
2026-04-02 09:57:21 -0300

    att pasta

--- js/scripttelacontato.js
+++ js/scripttelacontato.js
@@ -57,3 +57,47 @@ document.addEventListener("DOMContentLoaded", () => {
     subtree: true,
   });
 });
+
+//DOM no Formulário - Validação
+
+const form = document.querySelector("#fale-comigo");
+const erro = document.querySelector("#erro");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nome = document.querySelector("#nome").value;
+  const email = document.querySelector("#email").value;
+  const numero = document.querySelector("#number").value.trim();
+  const mensagem = document.querySelector("#mensagem").value;
+
+  //validação
+  if(nome === "") {
+    erro.textContent = "Nome Obrigatório!";
+    erro.style.color = "red";
+    return;
+  }
+
+  if(!email.includes("@")) {
+    erro.textContent = "Email Inválido!"
+    erro.style.color = "red"
+    return;
+  }
+
+  if(numero.length !== 11) {
+    erro.textContent = "Número deve ter 11 dígitos!"
+    erro.style.color = "red";
+    return;
+  }
+
+  //limpa erro
+  erro.textContent = "";
+
+  // exibe no console
+  console.log(nome);
+  console.log(email);
+  console.log(numero);
+  console.log(mensagem);
+
+  alert("Formulário enviado!");
+});
